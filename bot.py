1fbe1d311264c2749d57cfc1e4fedfd737d94b8a
Subat <ssultanov030101@gmail.com>
2024-08-21 20:27:24 +0600

    updated bot.py

--- bot.py
+++ bot.py
@@ -1,50 +1,73 @@
 import discord
-from discord.ext import commands
+from cfg import TOKEN
+from discord.ext import commands, tasks
 import aiohttp
 import io
-from cfg import *
+from googletrans import Translator
 
 # Set up intents and bot
 intents = discord.Intents.default()
 intents.message_content = True
-intents.members = True  # Enable member intent
+intents.members = True  # Ensure members intent is enabled
 bot = commands.Bot(command_prefix='!', intents=intents)
+
+# Initialize the translator
+translator = Translator()
+
+# Global variables to keep track of the last user and channel interacted with
+last_user = None
+last_channel = None
 
 @bot.event
 async def on_ready():
     print(f'We have logged in as {bot.user}')
+    send_avatar.start()
 
 @bot.event
 async def on_message(message):
+    global last_user, last_channel
+
     if message.author == bot.user:
         return
 
-    if bot.user.mentioned_in(message):
-        # Check if the message contains mentions
-        if message.mentions:
-            user = message.mentions[0]
+    # Update the last user and channel interacted with
+    if message.mentions:
+        # Update to the first mentioned user
+        last_user = message.mentions[0]
+        last_channel = message.channel
 
-            # Debug: Print user details
-            print(f'Mentioned user: {user.name}, Avatar URL: {user.avatar}')
-            
-            if user.avatar:
-                avatar_url = user.avatar.url
-                # Debug: Print the avatar URL
-                print(f'Avatar URL: {avatar_url}')
-                
-                async with aiohttp.ClientSession() as session:
-                    async with session.get(avatar_url) as response:
-                        if response.status == 200:
-                            img_data = io.BytesIO(await response.read())
-                            await message.channel.send(f'{user.name}\'s avatar:', file=discord.File(img_data, 'avatar.png'))
-                        else:
-                            await message.channel.send('Could not retrieve the avatar. Response status: {}'.format(response.status))
-            else:
-                await message.channel.send('User does not have an avatar set. Debug: Avatar URL: {}'.format(user.avatar.url if user.avatar else 'None'))
-        else:
-            await message.channel.send('No user mentioned.')
-    
+    # Detect message language
+    detected_lang = translator.detect(message.content).lang
+
+    # Translate message if necessary
+    if detected_lang == 'ru':
+        translated = translator.translate(message.content, src='ru', dest='en')
+        response = f'Translated message: {translated.text}'
+    elif detected_lang == 'en':
+        translated = translator.translate(message.content, src='en', dest='ru')
+        response = f'Переведенное сообщение: {translated.text}'
+    else:
+        response = 'Sorry, I can only translate between English and Russian.'
+
+    await message.channel.send(response)
     await bot.process_commands(message)
 
-# Replace 'YOUR_TOKEN_HERE' with your actual bot token
+@tasks.loop(seconds=10)
+async def send_avatar():
+    global last_user, last_channel
+
+    if last_user and last_channel:
+        avatar_url = last_user.avatar.url
+        
+        async with aiohttp.ClientSession() as session:
+            async with session.get(avatar_url) as response:
+                if response.status == 200:
+                    img_data = io.BytesIO(await response.read())
+                    # Send the avatar image to the last channel interacted with
+                    await last_channel.send(f'{last_user.name}\'s avatar:', file=discord.File(img_data, 'avatar.png'))
+                else:
+                    print(f'Failed to retrieve avatar. Status code: {response.status}')
+    else:
+        print('No user or channel available to send avatar.')
+
 bot.run(TOKEN)
